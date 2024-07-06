afe7822531034105ecf47082e9f33848b5174af0
Seisa <seisalikotsi@gmail.com>
2024-07-06 12:04:39 +0200

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,1 +1,11 @@
+function toggleDetails(detailsId) {
+    const detailsElement = document.getElementById(detailsId);
+    const displayStyle = detailsElement.style.display;
+
+    if (displayStyle === 'none') {
+        detailsElement.style.display = 'block';
+    } else {
+        detailsElement.style.display = 'none';
+    }
+}
 
